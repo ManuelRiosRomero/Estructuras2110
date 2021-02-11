7289bfd43df5eda3120bf377568e52e42f7eda17
ManuelRiosRomero <36652253+ManuelRiosRomero@users.noreply.github.com>
2021-02-11 11:11:46 -0500

    Add files via upload

---
+++ formatos/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+//g++ -std=c++11 main.cpp -o main
+// ./main
+// -------------------------------------------------------------------------
+int main( int argc, char* argv[] )
+{
+    std::cout<<"\n -Archivo de Prueba- \n";
+      
+
+  return( 0 );
+}
+
+
