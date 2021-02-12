3d291ad6c8e7a78f5b66883a5fe5baca329c43c5
ManuelRiosRomero <36652253+ManuelRiosRomero@users.noreply.github.com>
2021-02-11 21:02:43 -0500

    Add files via upload

--- Proyecto/main.cpp
+++ Proyecto/main.cpp
@@ -6,26 +6,26 @@
 
 using namespace std;
 int main( int argc, char* argv[] ){
-    char *comando = new char[40];
+    char *comando = new char[40], *parametro, *instruccion;
     bool bandera = true;
 
     while (bandera){
        
         cout<<"\n\n----------SCRABBLE----------";
         cout<<"\n-----Lista de Comandos------";
-        cout<<"\n1. inicializar diccionario.txt";
-        cout<<"\n2. inciar_inverso diccionario.txt";
-        cout<<"\n3. puntaje palabra";
+        cout<<"\n1. inicializar ARCHIVO";
+        cout<<"\n2. iniciar_inverso ARCHIVO";
+        cout<<"\n3. puntaje PALABRA";
         cout<<"\n";
         cout<<"\n ---/---/---/---/---/---/---";
-        cout<<"\n4. inciar_arbol diccionario.txt";
-        cout<<"\n5. inciar_arbol_inverso diccionario.txt";
-        cout<<"\n6. palabras_por_prefijo prefijo";
-        cout<<"\n7. palabras_por_sufijo sufijo";
+        cout<<"\n4. iniciar_arbol ARCHIVO";
+        cout<<"\n5. iniciar_arbol_inverso ARCHIVO";
+        cout<<"\n6. palabras_por_prefijo PREFIJO";
+        cout<<"\n7. palabras_por_sufijo SUFIJO";
         cout<<"\n";
         cout<<"\n ---/---/---/---/---/---/---";
         cout<<"\n8. grafo_de_palabras";
-        cout<<"\n9. posibles_palabras letras";
+        cout<<"\n9. posibles_palabras LETRAS";
         cout<<"\n0. salir";
         cout<<"\n#. ayuda";
         cout<<"\n-----------------------------\n";
@@ -33,67 +33,85 @@ int main( int argc, char* argv[] ){
         cout<< "\n$: ";
         cin.getline(comando, 40);
         
+           instruccion = strtok(comando, " ");
+           
         //cout<<comando;
-        
+     
         //-----------comando 1 incializar diccionario
-        if(strcmp(comando, "inicializar diccionario.txt")== 0){
+        if(strcmp(instruccion, "inicializar")== 0){
+        	parametro = strtok(NULL, " ");
+        	cout << "Parametro: " << parametro << endl;
             cout<<"\nSe entro al comando 1";
         }
         
         //-----------comando 2 incia inverso diccionario
-        else if(strcmp(comando,"inciar_inverso diccionario.txt")== 0){
+        else if(strcmp(instruccion,"iniciar_inverso")== 0){
+        	parametro = strtok(NULL, " ");
+        	cout << "Parametro: " << parametro << endl;
             cout<<"\nSe entro al comando 2";
         }
 
         //-----------comando 3 puntaje de palabra
-        else if(strcmp(comando, "puntaje palabra")== 0){
+        else if(strcmp(instruccion, "puntaje")== 0){
+        	parametro = strtok(NULL, " ");
+        	cout << "Parametro: " << parametro << endl;
             cout<<"\nSe entro al comando 3";
         }
 
         //-----------comando 4 entrega 2 inciiar arbol
-        else if(strcmp(comando,"inciar_arbol diccionario.txt")== 0){
+        else if(strcmp(instruccion,"iniciar_arbol")== 0){
+        	parametro = strtok(NULL, " ");
+        	cout << "Parametro: " << parametro << endl;
             cout<<"\nSe entro al comando 4";
         }
 
         //-----------comando 5 entrega 2 arbol inverso
-        else if(strcmp(comando, "inciar_arbol_inverso diccionario.txt")== 0){
+        else if(strcmp(instruccion, "iniciar_arbol_inverso")== 0){
+        	parametro = strtok(NULL, " ");
+        	cout << "Parametro: " << parametro << endl;
             cout<<"\nSe entro al comando 5";
         }
 
         //-----------comando 6 entrega 2 palabras prefijo
-        else if(strcmp(comando,"palabras_por_prefijo prefijo")== 0){
+        else if(strcmp(instruccion,"palabras_por_prefijo")== 0){
+        	parametro = strtok(NULL, " ");
+        	cout << "Parametro: " << parametro << endl;
             cout<<"\nSe entro al comando 6";
         }
 
         //-----------comando 7 entrega 2 palabras sufijo
-        else if(strcmp(comando, "palabras_por_sufijo sufijo")== 0){
+        else if(strcmp(instruccion, "palabras_por_sufijo")== 0){
+        	parametro = strtok(NULL, " ");
+        	cout << "Parametro: " << parametro << endl;
             cout<<"\nSe entro al comando 7";
         }
 
         //-----------comando 8 entrega 3 grafo de palabras
-        else if(strcmp(comando,"grafo_de_palabras")== 0){
+        else if(strcmp(instruccion,"grafo_de_palabras")== 0){
             cout<<"\nSe entro al comando 8";
         }
 
         //-----------comando 9 entrega 3 posibles palabras letras
-        else if(strcmp(comando, "posibles_palabras letras")== 0){
+        else if(strcmp(instruccion, "posibles_palabras")== 0){
+        	parametro = strtok(NULL, " ");
+        	cout << "Parametro: " << parametro << endl;
             cout<<"\nSe entro al comando 9";
         }
 
         //-----------comando de ayuda
-        else if(strcmp(comando, "ayuda")== 0){
+        else if(strcmp(instruccion, "ayuda")== 0){
             cout<<"\nSe entro al comando de ayuda";
         }
 
         //-----------Salir del ciclo
-        else if(strcmp(comando, "salir") == 0){
+        else if(strcmp(instruccion, "salir") == 0){
             bandera = false;
         }
         else{
             cout<<"\nComando no identificado...";
         }
         cout<<"\n\n";
-        cin.get();
+        
     }
 
 }
